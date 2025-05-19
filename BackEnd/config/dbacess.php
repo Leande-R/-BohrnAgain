af5f1eb3e4ec03a542f164b6a0b23c5defef3f5a
Leande-R <131276205+Leande-R@users.noreply.github.com>
2025-05-19 20:59:28 +0200

    BackendFixesForIndexHTML

--- BackEnd/config/dbacess.php
+++ BackEnd/config/dbacess.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db = 'bohrnagaindb';
 $user = 'root';
-$pass = 'root'; 
+$pass = ''; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
--- BackEnd/logic/productController.php
+++ BackEnd/logic/productController.php
@@ -1,17 +1,9 @@
 <?php
 require_once '../config/dbacess.php';  
 require_once '../models/product.class.php';
-$statement = $pdo->query("SELECT * FROM products");
-$products = [];
 
-while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-    $products[] = new Product(
-        $row['id'],
-        $row['name'],
-        $row['description'],
-        $row['price'],
-        $row['image'],
-        $row['rating']
-    );
-}
+$products = Product::getAll($pdo);  // Logik wandert in die Klasse
+
+header('Content-Type: application/json');
+echo json_encode(array_map(fn($p) => $p->toArray(), $products));
 ?>
